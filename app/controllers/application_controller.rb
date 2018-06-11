0d57db9c42fbd6331edbeaa65874493fa48bb710
Scott B <scottberke@gmail.com>
2018-06-11 15:01:42 -0600

    Add chat controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
-class ApplicationController < ActionController::API
-  before_action :authenticate_uer!
+class ApplicationController < ActionController::API  
+  before_action :authenticate_user!
 
-  protect_from_forgery with: :exception
+
 end
--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,4 +1,6 @@
 class ChatController < ApplicationController
   def index
+    other_users = User.everyone_else(current_user)
+    #conversations = Conversations.includes(recipient, messages).find(conversation_ids)
   end
 end
