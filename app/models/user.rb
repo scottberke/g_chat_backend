eec4b637a3766c82d7e9dc9f8fa41610d9ced2b3
Scott B <scottberke@gmail.com>
2018-06-11 15:01:09 -0600

    Update user and spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :messages
+  has_many :chats, foreign_key: :sender_id
+
+  scope :everyone_else, ->(user) { all.where.not(id: user.id) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { User.new }
 
   it { is_expected.to respond_to :email }
   it { is_expected.to respond_to :encrypted_password }
 
+  describe 'relation to messages' do
+    it 'has many messages' do
+      expect(user).to have_many :messages
+    end
+  end
+
+  describe 'relation to chats' do
+    it 'has many chats' do
+      expect(user).to have_many :chats
+    end
+  end
+
+  describe '.everyone_else' do
+    let!(:user_1) { FactoryBot.create(:user) }
+    let!(:user_2) { FactoryBot.create(:user) }
+
+    it 'returns the other user' do
+      everyone_else = User.everyone_else(user_1)
+      expect(everyone_else[0]).to eq user_2
+    end
+
+    it 'returns ONLY the other user' do
+      everyone_else = User.everyone_else(user_1)
+      expect(everyone_else.size).to eq 1
+    end
+  end
 end
