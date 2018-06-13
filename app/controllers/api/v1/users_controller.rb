eb67d1f226025da6052ebf87440031890acb7752
Scott B <scottberke@gmail.com>
2018-06-13 10:49:21 -0600

    Add initial wiring for doorkeeper with devise

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::UsersController < ApplicationController
-  skip_before_action :authenticate_user!
+
   def index
-    # binding.pry
     current_user = User.find(1)
     other_users = User.everyone_else(current_user)
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
-class ApplicationController < ActionController::API  
-  before_action :authenticate_user!
+class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :doorkeeper_authorize!
+  respond_to :json
+
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:email]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  private
+
+  # For Doorkeeper
+  def current_resource_owner
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
 
 end
