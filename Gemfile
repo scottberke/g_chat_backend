8b7af9fffe5cf8572f5a8236360ab6bb4a914b01
Scott B <scottberke@gmail.com>
2018-06-13 09:53:40 -0600

    Update gemfile and config to support cors

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'rails', '~> 5.1.6'
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
-
+gem 'rack-cors'
 gem 'devise'
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'users/index'
+      resources :chats, only: [:create]
+    end
+  end
+
   devise_for :users
 
-  root 'chat#index'
+  # root 'chat#index'
+
+  mount ActionCable.server => '/cable'
 end
