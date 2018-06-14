78e568e35902402df1f6cbce86af1b2844e5c31f
Scott B <scottberke@gmail.com>
2018-06-14 15:38:18 -0600

    Try fixing CORS issue with heroku

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,11 @@ module GChat
       end
     end
 
+    config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => 'shrouded-wave-69866.herokuapp.com',
+    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+  }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
