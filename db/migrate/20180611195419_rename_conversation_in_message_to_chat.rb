2035efd60ad06833df6617ad39d8bd7959247e23
Scott B <scottberke@gmail.com>
2018-06-14 14:34:59 -0600

    bad scott

--- db/migrate/20180611195419_rename_conversation_in_message_to_chat.rb
+++ db/migrate/20180611195419_rename_conversation_in_message_to_chat.rb
@@ -1,5 +1,5 @@
 class RenameConversationInMessageToChat < ActiveRecord::Migration[5.1]
   def change
-    rename_column :messages, :conversation_id, :chat_id
+    # rename_column :messages, :conversation_id, :chat_id
   end
 end
