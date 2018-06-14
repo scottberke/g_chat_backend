165b36755c4895f9eb0fd301feb07fa45a6faaad
Scott B <scottberke@gmail.com>
2018-06-14 14:33:39 -0600

    eek

--- db/migrate/20180611192456_create_messages.rb
+++ db/migrate/20180611192456_create_messages.rb
@@ -3,7 +3,7 @@ class CreateMessages < ActiveRecord::Migration[5.1]
     create_table :messages do |t|
       t.text :body
       t.references :user, foreign_key: true
-      t.references :chat, foreign_key: true
+      # t.references :chat, foreign_key: true
 
       t.timestamps
     end
